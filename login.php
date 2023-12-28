d1f1247ac4deabf56f30c55b4abb2d44a9637e53
Steven Fischer <40443261+steven-fischer@users.noreply.github.com>
2023-12-27 20:12:40 -0800

    Inital Upload

---
+++ login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Harbor Art | Login Successful</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+<!--
+
+
+
+-->
+
+<style>
+
+
+</style>
+
+</head>
+<body>
+<?php
+
+//Get data from a GET or POST (change GET to POST for post)
+
+if(isset($_GET['data'])) { $data = $_GET['data']; } else { $data = ""; }
+
+
+print "(body of your project) $data";
+
+?>
+</body>
+</html>
---
+++ phpTemplate.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>CID Assignment</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+<!--
+
+CID Name Assignment
+
+What objectives are you demonstrating?
+
+(place here)
+
+-->
+
+<style>
+
+
+</style>
+
+</head>
+<body>
+<?php
+
+//Get data from a GET or POST (change GET to POST for post)
+
+if(isset($_GET['data'])) { $data = $_GET['data']; } else { $data = ""; }
+
+
+print "(body of your project) $data";
+
+?>
+</body>
+</html>
